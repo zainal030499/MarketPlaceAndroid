8d61b2fe937b19cafdd09cbff0b712edaac5986e
user <zainal0399@gmail.com>
2022-10-24 15:54:32 +0700

    init

---
+++ app/src/main/java/com/example/maketplace/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.maketplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
